8b864a1d9f6a9de0e11f2935767e9214fc653728
johndepatie <john.depatie1@gmail.com>
2020-08-19 20:45:32 -0600

    Added form and route to handle

--- controllers/accounts.js
+++ controllers/accounts.js
@@ -3,3 +3,9 @@ var express = require('express');
 exports.signupForm = function(req, res) {
     res.render('accounts/new');
 };
+
+exports.create = function(req, res) {
+    console.log(req.body)
+
+    res.end
+};
