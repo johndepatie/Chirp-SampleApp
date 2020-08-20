b40d2a280ded4639863e88120ae4ca7b7b0553cc
johndepatie <john.depatie1@gmail.com>
2020-08-19 18:24:09 -0600

    Commented out MongoDB for testing on Heroku

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
 
+/*
 const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017';
 
@@ -16,7 +17,7 @@ MongoClient.connect(url, function(err, client) {
     console.log(documents);
     client.close();
   });
-});
+});*/
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
