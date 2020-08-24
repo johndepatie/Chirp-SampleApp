a5240860ee265a784a2e945850e35354b7eaa80e
johndepatie <john.depatie1@gmail.com>
2020-08-23 21:05:19 -0600

    Created dev branch

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/users');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Chirp' });
+
+  /* Throws user data from the database. Literally never actually do this
+  User.find(function (err, users){
+    if (err) console.log(err)
+
+    res.render('accounts/list', { accounts: users })
+  });*/
+  
 });
 
 module.exports = router;
